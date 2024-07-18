5c359e49dfa879cb234d618e374463352a1b4f09
3msinhvien <dotung.ptit@gmail.com>
2024-07-18 23:27:57 +0700

    fix database khong nhan duoc thong tin

--- src/config.js
+++ src/config.js
@@ -1,24 +1,23 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/Login");
+const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
 
-connect.then(() =>{
+connect.then(() => {
     console.log("Database connected Success");
 })
 .catch(() => {
-    console.log("Database can't connected");
+    console.log("Database cannot be connected!");
 });
 
-const loginSchema = new mongoose.Schema({
+const LoginSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
     password: {
-        type: String,
+        type:String, 
         required: true
     }
 });
 
-const collection = new mongoose.model("user", loginSchema);
-
+const collection = new mongoose.model("users" ,LoginSchema);
 module.exports = collection;
